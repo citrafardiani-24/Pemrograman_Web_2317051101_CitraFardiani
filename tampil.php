44fc6e1d42f99f003565e17300eb2eb2e2f48aa9
citrafardiani-24 <148729858+citrafardiani-24@users.noreply.github.com>
2025-05-10 14:40:27 +0700

    Create tampil.php

---
+++ tampil.php
@@ -0,0 +1,14 @@
+<?php
+    $nama = $_POST["nama"];
+    $usia = $_POST["usia"];
+    $NPM = $_POST["NPM"];
+    $alamat = $_POST["alamat"];
+
+    echo "Nama Anda adalah " . $nama;
+    echo "<br>";
+    echo "Usia Anda adalah " . $usia;
+    echo "<br>";
+    echo "NPM Anda adalah " . $NPM;
+    echo "<br>";
+    echo "Alamat Anda adalah " . $alamat;
+?>
