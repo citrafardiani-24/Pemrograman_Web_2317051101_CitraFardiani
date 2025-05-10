f7b5d84f5236debe06c84ff1a3710cef766fc03f
citrafardiani-24 <148729858+citrafardiani-24@users.noreply.github.com>
2025-05-10 14:36:56 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="post" action="tampil.php">
+        <label>Masukkan nama</label>
+        <input type="text" name="nama">
+        <br><br>
+        <label>Masukkan usia</label>
+        <input type="text" name="usia">
+        <br><br>
+        <label>Masukkan NPM</label>
+        <input type="text" name="NPM">
+        <br><br>
+        <label>Masukkan alamat</label>
+        <input type="text" name="alamat">
+        <br><br>
+        <input type="submit">
+    </form>
+</body>
+</html>
